7fcbafc9a820f64f5b97c0de358379e05be291f8
gustojvalle <gustojvalle@outlook.com>
2022-11-21 14:08:13 +0000

    Develop (#12)
    * removed unused search context
    
    * added tests to github actions
    
    * added tests to github actions
    
    * updated workflow name to workfloes
    
    * updated node version on tests
    
    * updated test ci
    
    * Update tests.yaml
    
    * specified node version on deployment
    
    * specified node engine
    
    * sdfs
    
    * matched node version in deploy and package
    
    * update
    
    * fixed no engines
    
    * removed test from pdocution deploy
    
    * fixed node version on test
    
    * fixed test on main.yaml
    
    * updated main.yaml to install packages before testing
    
    * fixed merging conflict
    
    * fixed some style and cleaned the code
    
    * fixed some styling
    
    * added conditional to results per page and emojis
    
    * fixed merge conflict leftover
    
    * added vercel ignore and storybooks
    
    * added addons to use images on storybooks
    
    * fixed type any in the code
    
    * removed package-lock
    
    * removed leftover merge

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,17 +8,12 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next-router",
-<<<<<<< HEAD
     "storybook-addon-next",
-=======
->>>>>>> d8c37cd960f4c2ef48732ca228eee99e544ae026
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
-<<<<<<< HEAD
   staticDirs: ["../public"],
-=======
->>>>>>> d8c37cd960f4c2ef48732ca228eee99e544ae026
+
 };
